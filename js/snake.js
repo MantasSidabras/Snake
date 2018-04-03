17988a38603c0d0949549e7abea1c4fac0dd7019
MantasSidabras <sid.mantas@gmail.com>
2018-04-04 00:41:18 +0300

    another input fix

--- js/snake.js
+++ js/snake.js
@@ -2,18 +2,27 @@ var Snake = function(x, y) {
   this.length = 1;
   this.xSpeed = 1;
   this.ySpeed = 0;
+
+  this.oldXSpeed = 1;
+  this.oldYSpeed = 0;
   this.eatenFood = [];
   this.body = [
     game.add.sprite(x, y, block('#b3ffff')),
-    game.add.sprite(x-scale, y, block('#ffffff')),
-    game.add.sprite(x-2*scale, y, block('#ffffff')),
+    game.add.sprite(x - scale, y, block('#ffffff')),
+    game.add.sprite(x - 2 * scale, y, block('#ffffff')),
   ];
 
   this.changeDirection = function(xdir, ydir) {
-    if (this.xSpeed != xdir && this.xSpeed != -xdir)
+    if (this.oldXspeed != xdir && this.oldXSpeed != -xdir){
       this.xSpeed = xdir;
-    if (this.ySpeed != ydir && this.ySpeed != -ydir)
+    } else {
+      this.xSpeed = this.oldXSpeed;
+    }
+    if (this.oldYSpeed != ydir && this.oldYSpeed != -ydir){
       this.ySpeed = ydir;
+    } else {
+      this.ySpeed = this.oldYSpeed;
+    }
   }
   this.update = function() {
     for (let i = this.body.length - 1; i > 0; i--) {
@@ -22,8 +31,12 @@ var Snake = function(x, y) {
     }
     this.body[0].x += this.xSpeed * scale;
     this.body[0].y += this.ySpeed * scale;
+
+    this.oldXSpeed = this.xSpeed;
+    this.oldYSpeed = this.ySpeed;
+
     this.death();
-    if(this.body[0].x == food.x && this.body[0].y == food.y){
+    if (this.body[0].x == food.x && this.body[0].y == food.y) {
       score++;
       food.destroy();
       let location = foodLocation();
@@ -34,26 +47,26 @@ var Snake = function(x, y) {
 
     let foodToShift = 0;
     this.eatenFood.forEach((f) => {
-      if(typeof f !== 'undefined'){
+      if (typeof f !== 'undefined') {
         f.timeToGrow++;
 
-        if(f.timeToGrow >= this.body.length){
+        if (f.timeToGrow >= this.body.length) {
           foodToShift++;
         }
       }
     });
-    for(let i = 0; i < foodToShift; i++){
+    for (let i = 0; i < foodToShift; i++) {
       this.grow(this.eatenFood.shift());
     }
 
 
   }
 
-  this.death = function(){
-    if(this.body[0].x >= properties.width || this.body[0].x < 0 || this.body[0].y >= properties.height || this.body[0].y < 3 * scale)
+  this.death = function() {
+    if (this.body[0].x >= properties.width || this.body[0].x < 0 || this.body[0].y >= properties.height || this.body[0].y < 3 * scale)
       gameover();
-    for(let i = 2; i < this.body.length; i++){
-      if(this.body[0].x == this.body[i].x && this.body[0].y == this.body[i].y){
+    for (let i = 2; i < this.body.length; i++) {
+      if (this.body[0].x == this.body[i].x && this.body[0].y == this.body[i].y) {
         gameover();
       }
     }
@@ -65,7 +78,11 @@ var Snake = function(x, y) {
     let foodX = x;
     let foodY = y;
 
-    this.eatenFood.push({timeToGrow, foodX, foodY});
+    this.eatenFood.push({
+      timeToGrow,
+      foodX,
+      foodY
+    });
   }
   this.grow = function(object) {
     this.body.push(game.add.sprite(object.foodX, object.foodY, block('#ffffff')));
@@ -76,7 +93,7 @@ var Snake = function(x, y) {
 
 
 var block = function(color) {
-  let bmd = game.add.bitmapData(0,0);
+  let bmd = game.add.bitmapData(0, 0);
   bmd.ctx.beginPath();
   bmd.ctx.rect(0, 0, scale, scale);
   bmd.ctx.fillStyle = color;
